eeb99100a833dde90f326a75bca2c74386fcf51b
Harshitha manneri <harshitha.manneri@gmail.com>
2026-02-16 04:52:16 +0000

    https://ik.imagekit.io/cgkpgpdr2/icon.png add this image to diplay favic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,9 @@ export const metadata: Metadata = {
   title: 'Harshitha Manneri',
   description: 'AI Systems | Deep Learning Specialist',
   icons: {
-    icon: 'https://ik.imagekit.io/cgkpgpdr2/professional_photo.jpg?updatedAt=1771062645167',
-    shortcut: 'https://ik.imagekit.io/cgkpgpdr2/professional_photo.jpg?updatedAt=1771062645167',
-    apple: 'https://ik.imagekit.io/cgkpgpdr2/professional_photo.jpg?updatedAt=1771062645167',
+    icon: 'https://ik.imagekit.io/cgkpgpdr2/icon.png',
+    shortcut: 'https://ik.imagekit.io/cgkpgpdr2/icon.png',
+    apple: 'https://ik.imagekit.io/cgkpgpdr2/icon.png',
   },
 };
 
